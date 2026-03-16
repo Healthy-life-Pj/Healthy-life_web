e0fbd0744fc76f19681bf237b4b3146d3b10238b
als5855 <wlals5855@naver.com>
2026-03-16 23:43:13 +0900

    update:auth

--- healthy-life/src/views/auth/login/find-id/FindId.tsx
+++ healthy-life/src/views/auth/login/find-id/FindId.tsx
@@ -1,20 +1,22 @@
-import { Box, Button, InputAdornment } from '@mui/material';
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import "../../../../style/auth/login/login.css"
-import ReactModal from 'react-modal';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { FIND_ID, FIND_ID_BY_TOKEN, MAIL_PATH, MAIN_APT_PATH } from '../../../../constants';
+import { Box, Button, InputAdornment } from "@mui/material";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import "../../../../style/auth/login/login.css";
+import ReactModal from "react-modal";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import {
+  FIND_ID,
+  MAIL_PATH,
+  MAIN_APT_PATH,
+} from "../../../../constants";
 
 function FindId() {
   const [formData, setFormData] = useState({
     name: "",
     userEmail: "",
   });
-  const [modal, setModal] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  const [modalMessage, setModalMessage] = useState<string>('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const element = e.target;
@@ -24,7 +26,7 @@ function FindId() {
   const handleSendMail = async (
     e:
       | React.KeyboardEvent<HTMLInputElement>
-      | React.MouseEvent<HTMLButtonElement>
+      | React.MouseEvent<HTMLButtonElement>,
   ) => {
     if ("key" in e && e.key !== "Enter") return;
     if (!formData.name || !formData.userEmail) {
@@ -32,71 +34,50 @@ function FindId() {
     }
     setLoading(true);
     try {
-      const response = await axios.post(`${MAIN_APT_PATH}${MAIL_PATH}${FIND_ID}`, formData);
+      const response = await axios.post(
+        `${MAIN_APT_PATH}${MAIL_PATH}${FIND_ID}`,
+        formData,
+      );
       const userData = response.data.data;
       if (userData) {
-        setModalMessage("이메일 전송이 완료됐습니다.");
+        alert("이메일 전송이 완료됐습니다.");
       }
-      setModal(true);
     } catch (error) {
       console.error(error);
-        setModalMessage(`이메일 전송에에 실패했습니다. 다시 시도해주세요.`);
-        setModal(true);
+      alert(`이메일 전송에에 실패했습니다. 다시 시도해주세요.`);
     } finally {
       setLoading(false);
     }
   };
-  const closeModal = () => {
-    setModal(false);
-  }
 
   return (
-    <div className='findIdContainer'>
+    <div className="findIdContainer">
       <h1>아이디 찾기</h1>
       <Box>
-      <form className='findIdForm'>
-      <TextField
-        label="이름"
-        id='userName'
-        sx={{ m: 1, width: '25ch'}}
-        InputProps={{
-        startAdornment: <InputAdornment position='start'>이름</InputAdornment>
-        }}
-        value={formData.name}
-        name='name'
-        onChange={handleChange}
-      />
-      <TextField
-      label="email"
-      id='userPhone'
-      sx={{ m: 1, width: '25ch'}}
-      InputProps={{
-      startAdornment: <InputAdornment position='start'>이메일</InputAdornment>
-      }}
-      value={formData.userEmail}
-      name='userEmail'
-      onChange={handleChange}
-      />
-      <button className='loginButton' onClick={handleSendMail}>
-        {loading ? <p>전송중...</p>: <p>아이디 찾기</p> }
-      </button>
-      </form>
-      <ReactModal
-        isOpen={modal}
-        onRequestClose={closeModal}
-        className="modalContent"
-        overlayClassName="modalOverlay"
-      >
-      <h2>알림</h2>
-        <div className="findUserIdModal">
-        <p>{modalMessage}</p>
-        <button onClick={closeModal} className="closeModalBtn">닫기</button>
-        </div>
-      </ReactModal>
+        <form className="findIdForm">
+          <TextField
+            label="이름"
+            id="userName"
+            sx={{ m: 1, width: "25ch" }}
+            value={formData.name}
+            name="name"
+            onChange={handleChange}
+          />
+          <TextField
+            label="email"
+            id="userPhone"
+            sx={{ m: 1, width: "25ch" }}
+            value={formData.userEmail}
+            name="userEmail"
+            onChange={handleChange}
+          />
+          <button type="button" className="loginButton" onClick={handleSendMail}>
+            {loading ? <p>전송중...</p> : <p>아이디 찾기</p>}
+          </button>
+        </form>
       </Box>
     </div>
-    
-  )
+  );
 }
 
-export default FindId
+export default FindId;
