a75a1558057121464f9c2f67a07da6ea4f5c2c6a
als5855 <wlals5855@naver.com>
2026-05-05 14:49:47 +0900

    deploy-ready

--- healthy-life/src/constants/api.ts
+++ healthy-life/src/constants/api.ts
@@ -1,6 +1,6 @@
 export const MAIN_APT_PATH = 
     process.env.NODE_ENV === "production"
-    ? "https://healthy-life-web-eta.vercel.app"
+    ?  "https://healthy-life-was.onrender.com"
     : "http://localhost:4040";
 
 
